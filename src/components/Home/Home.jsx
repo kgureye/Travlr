91cc5d82028c2e7f355a9c8a9307b98418a8901d
kgureye <kanigureye@outlook.com>
2023-02-03 22:47:50 +0100

    updated minor changes

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './home.css'
 import video from '../../assets/video.mp4'
 import { GrLocation } from 'react-icons/gr'
@@ -9,7 +9,17 @@ import { SiTripadvisor } from 'react-icons/si'
 import { BsListTask } from 'react-icons/bs'
 import { TbApps } from 'react-icons/tb'
 
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+
+
 const Home = () => {
+
+  // scroll animation w/ useEffect
+  useEffect(() => {
+    Aos.init({duration: 2000})
+  }, [])
+
   return (
     <section className="home">
       <div className="overlay">
@@ -20,15 +30,15 @@ const Home = () => {
 
       <div className="homeContent container">
         <div className="textDiv">
-          <span className="smallText">
+          <span data-aos="fade-up" className="smallText">
             Våre pakkereiser
           </span>
-          <h1 className="homeTitle">
+          <h1 data-aos="fade-up" className="homeTitle">
              Finn din neste reise
           </h1>
         </div>
 
-        <div className="cardDiv grid">
+        <div data-aos="fade-up" className="cardDiv grid">
           <div className="destinationInput">
               <label htmlFor="city">Søk etter destinasjon:</label>
               <div className="input flex">
@@ -66,18 +76,7 @@ const Home = () => {
             </div>
         </div>
 
-        <div className="homeFooterIcons flex">
-          <div className="rightIcons">
-             <FiFacebook className="icon" />
-             <AiOutlineInstagram className="icon" />
-             <SiTripadvisor className="icon" />
-          </div>
-
-          <div className="leftIcons">
-             <BsListTask className="icon" />
-             <TbApps className="icon" />
-          </div>
-        </div>
+        
       </div>
 
 
