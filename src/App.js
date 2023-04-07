9af12bf8fc6cc3400ecd26becd6e70628c253254
kani gureye <kanigureye@outlook.com>
2023-04-07 17:35:30 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
+import './app.css' 
 import React from 'react'
-import './app.css'
 import Navbar from './components/Navbar/Navbar'
 import Home from './components/Home/Home'
 import Main from './components/Main/Main'
